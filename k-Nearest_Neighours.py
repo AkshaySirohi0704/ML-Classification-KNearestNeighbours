39b1e9e51d6a6860c5d1b6a3c193d87ece9f6847
Shane0704 <49399876+Shane0704@users.noreply.github.com>
2022-06-16 17:04:18 +0530

    First Push, K-NearestNeibors Classification

---
+++ k-Nearest_Neighours.py
@@ -0,0 +1,37 @@
+# Libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Data Processing
+data = pd.read_csv('Social_Network_Ads.csv')
+X = data.iloc[:, :-1].values
+y = data.iloc[:, -1].values
+# Splitting Data
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+print('-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-')
+print(' splitting . . . ')
+
+# Training
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
+classifier.fit(X_train, y_train)
+print('-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-')
+print(' Training . . . ')
+
+# Predicitng
+print('-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-')
+print(' Predicting . . . ')
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print('-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-')
+print(' Confusion Matrix . . . ')
+print(cm)
+print('-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-')
+print('Accuracy Score : ', accuracy_score(y_test, y_pred))
+
